43181e87df0742fdb7a02b0f4edd3dd5c8cc3cff
martin6724 <martin.michelleann@gmail.com>
2018-05-06 10:32:37 -0400

    updated user model and fixed password migration error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'users/new'
+  resource :users
   resources :albums
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

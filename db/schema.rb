e2c0dcd236730127830a3f1897f281aab14872a8
martin6724 <martin.michelleann@gmail.com>
2018-05-06 15:00:59 -0400

    fixed login bug and image width. going live

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_05_06_183657) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "albums", force: :cascade do |t|
     t.string "title"
     t.string "image"
